3dc651fe5a3b5b795088289ae6ccf031581035c0
Murtja <murtja.alkhabbaz@uoit.net>
2017-03-24 20:20:39 -0400

    added and refactor code to ask user to enter 4 integers

--- src/NumberListApplication.java
+++ src/NumberListApplication.java
@@ -1,15 +1,42 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class NumberListApplication {
-	public static void main (String[]args){
-		NumberList myNumber = new NumberList();
-		myNumber.addValue(1);
-		myNumber.addValue(100000);
 
+	protected static int getSingleValue(){
+		Scanner in = new Scanner(System.in);
+		return in.nextInt();
+	}
+
+	protected static void getMultipleNumbersIntoNumberList(int integerGetCount, NumberList numberList) {
+		int validCount = 0;
+
+		while(validCount < integerGetCount) {
+			try {
+				System.out.println("Please enter an integer #" + (validCount + 1) + " :>");
+				int val1 = getSingleValue();
+				numberList.addValue(val1);
+				validCount++;
+			} catch (InputMismatchException e) {
+				System.out.println("That was not a valid integer. Please try again.");
+			}
+		}
+	}
+
+	protected static void printLargestSmallest(NumberList numberList){
 		try {
-			System.out.println("Smallest value = " + myNumber.getSmallest());
-			System.out.println("Largest value = " + myNumber.getLargest());
+			System.out.println("Smallest value = " + numberList.getSmallest());
+			System.out.println("Largest value = " + numberList.getLargest());
 		} catch (NoNumbersSeenException e){
 			System.out.println("The Number List has not yet seen any values to give you a smallest or largest number.");
 		}
+	}
+
+	public static void main (String[]args){
+		NumberList numberList = new NumberList();
+
+		getMultipleNumbersIntoNumberList(4, numberList);
+
+		printLargestSmallest(numberList);
 	}
 }
